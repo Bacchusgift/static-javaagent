3f3a81e5c31e286eaa73679405009ef09370c1fe
aster42 <mxl73@outlook.com>
2021-04-23 21:55:09 +0800

    update init

--- src/main/java/com/sample/agent/startup/SpringBeanInitializationProfiler.java
+++ src/main/java/com/sample/agent/startup/SpringBeanInitializationProfiler.java
@@ -2,23 +2,27 @@ package com.sample.agent.startup;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 public class SpringBeanInitializationProfiler {
     private static Map<Object, Map<String, Long>> applicationContextBeginTimeMap = new ConcurrentHashMap<>();
 
     public static void beforeInitialize(Object applicationContext, String beanName) {
+        Logger logger = Logger.getLogger("start");
         applicationContextBeginTimeMap.compute(applicationContext, (ctx, beginTimeMap) -> {
             if (beginTimeMap == null) {
                 beginTimeMap = new ConcurrentHashMap<>();
             }
             if (beginTimeMap.put(beanName, System.currentTimeMillis()) != null) {
                 // log
+                logger.warning(beanName + " init.");
             }
             return beginTimeMap;
         });
     }
 
     public static void afterInitialize(Object applicationContext, String beanName) {
+        Logger logger = Logger.getLogger("start");
         applicationContextBeginTimeMap.compute(applicationContext, (ctx, beginTimeMap) -> {
             if (beginTimeMap != null && beginTimeMap.get(beanName) != null) {
                 long st = beginTimeMap.get(beanName);
--- src/main/java/com/sample/agent/startup/StartupProfilerAgent.java
+++ src/main/java/com/sample/agent/startup/StartupProfilerAgent.java
@@ -9,10 +9,33 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class StartupProfilerAgent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
+        Logger logger = Logger.getLogger("start");
+        try {
+            FileHandler fh = new FileHandler("~/logs/log.data");
+            fh.setFormatter(new Formatter() {
+                @Override
+                public String format(LogRecord record) {
+                    SimpleDateFormat logTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    return String.format("%s [] %s - %s\n",
+                        logTime.format(new Date()), record.getLevel(), record.getMessage()
+                    );
+                }
+            });
+            logger.addHandler(fh);
+            logger.warning("logger init.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         inst.addTransformer(new DefaultTransformer(), true);
     }
 
@@ -20,7 +43,6 @@ public class StartupProfilerAgent {
         @Override
         public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                                 ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-
             try {
                 if (className != null && className.endsWith("ApplicationContextAwreProcessor")) {
                     CtClass ctClass = ClassPool.getDefault().makeClass(new ByteArrayInputStream(classfileBuffer));
@@ -31,11 +53,9 @@ public class StartupProfilerAgent {
                     CtMethod afterMethod = ctClass.getDeclaredMethod("postProcessAfterInitialization");
                     afterMethod.insertAfter("com.sample.agent.startup.SpringBeanInitializationProfiler.afterInitialize(this.applicationContext, beanName);");
                 }
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
             return classfileBuffer;
         }
     }
